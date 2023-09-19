2b0c349a3e2d3b5f747f43737bbd570c813891dc
Abdul Rehman <129746474+Abdul-Rehman-13812@users.noreply.github.com>
2023-09-19 13:44:43 +0500

    basics
    number, string, boolean ,function, any

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+let number = 3;
+console.log(number);
+let greeding = "Aslam O Alikum";
+console.log(greeding);
+greeding.toUpperCase();
+console.log(greeding);
+let num = 381;
+console.log(num);
+let isloogin = false;
+console.log(isloogin);
+function addtwonum(num) {
+    return num + 2;
+}
+addtwonum(5);
+console.log(num);
+function sigup(name, email, ispaid) { }
+sigup("Abdulrehman", "arifabdulrehman37@gmail.com", true);
---
+++ index.ts
@@ -0,0 +1,28 @@
+let  number  = 3;
+console.log(number);
+
+ let greeding : string = "Aslam O Alikum";
+ console.log(greeding);
+
+ greeding.toUpperCase()
+
+ console.log(greeding);
+
+ let num : number = 381 ;
+ console.log (num);
+
+ let isloogin : boolean = false;
+ console.log(isloogin);
+ 
+
+ function addtwonum(num :number){
+    return num+2;
+
+ }
+addtwonum(5);
+console.log(num);
+
+function sigup (name:string , email:string , ispaid:boolean)
+{}
+
+sigup("Abdulrehman","arifabdulrehman37@gmail.com",true);
